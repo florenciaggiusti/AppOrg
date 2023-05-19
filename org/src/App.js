3c36ec58dc5fafcd2ac967e48777296d84d8470d
Flowerncia <18071748-Flowerncia@users.noreply.replit.com>
2023-05-19 05:22:27 +0000

    Initial commit

---
+++ org/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Header from './componentes/Header/Header.js';
+import Formulario from './componentes/Formulario/Formulario.js'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Formulario />
+    </div>
+  );
+}
+
+export default App;
